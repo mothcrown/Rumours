03759673d37a28ba3957e95ac414d1342fde707a
Marcos <uchodc@gmail.com>
2018-01-29 23:14:20 +0000

    Moving NPCs!

--- src/js/game/play.js
+++ src/js/game/play.js
@@ -5,8 +5,9 @@ import images from './images'
 import './../phaser.min'
 
 let player
-let npcs
+let guests
 let cursors
+let timer
 
 function createPlayer(playState) {
   player = playState.game.add.sprite(320, 400, 'player')
@@ -39,33 +40,79 @@ function playerMovement() {
   }
 }
 
-function generateNPCs() {
-  const npc = npcs.create(200, 200, 'guestmale1')
-  npc.animations.add('walksouth', [0], 30, true)
-  npc.animations.add('walknorth', [1], 30, true)
-  npc.animations.add('walkwest', [2], 30, true)
-  npc.animations.add('walkeast', [3], 30, true)
+function guestsMovement(game) {
+  guests.forEach((guest) => {
+    const guest1 = guest
+    guest1.body.velocity.y = 0
+    guest1.body.velocity.x = 0
+    const roll = Math.floor((Math.random() * 10) + 1)
+    switch (roll) {
+      // South
+      case 1:
+        guest1.body.velocity.y = 75
+        guest1.animations.play('walksouth')
+        break
+      // Left
+      case 2:
+        guest1.body.velocity.x = -75
+        guest1.animations.play('walkwest')
+        break
+      // Up
+      case 3:
+        guest1.body.velocity.y = -75
+        guest1.animations.play('walknorth')
+        break
+      // Right
+      case 4:
+        guest1.body.velocity.x = 75
+        guest1.animations.play('walkeast')
+        break
+      default:
+        break
+    }
+  })
+  timer = game.time.now
 }
 
-function createNPCs(playState) {
-  npcs = playState.game.add.group()
-  npcs.physicsBodyType = Phaser.Physics.ARCADE
-  npcs.enableBody = true
-  npcs.setAll('anchor.x', 0.5)
-  npcs.setAll('anchor.y', 0.5)
-  npcs.setAll('checkWorldBounds', true)
-  npcs.setAll('body.collideWorldBounds', true)
-  npcs.setAll('body.immovable', true)
-  npcs.setAll('allowGravity', false)
-  generateNPCs()
-}
-
-function bumpIntoPeople(player1, npc) {
-  if (player1.body.y > npc.body.y + 20) {
-    player1.bringToTop()
+function generateGuests(playState) {
+  for (let i = 0; i < 5; i += 1) {
+    const guest = guests.create(playState.game.world.randomX, playState.game.world.randomY, 'guestmale1')
+    guest.anchor.set(0.5, 0.5)
+    guest.body.collideWorldBounds = true
+    guest.animations.add('walksouth', [0], 30, true)
+    guest.animations.add('walknorth', [1], 30, true)
+    guest.animations.add('walkwest', [2], 30, true)
+    guest.animations.add('walkeast', [3], 30, true)
   }
-  if (player1.body.y + 20 < npc.body.y) {
-    player1.sendToBack()
+}
+
+function createGuests(playState) {
+  guests = playState.game.add.group()
+  guests.physicsBodyType = Phaser.Physics.ARCADE
+  guests.enableBody = true
+  generateGuests(playState)
+}
+
+function blockMoving(char) {
+  const sprite = char
+  if (sprite.body.touching.up) {
+    sprite.body.velocity.y = 150
+  } else if (sprite.body.touching.down) {
+    sprite.body.velocity.y = -150
+  } else if (sprite.body.touching.left) {
+    sprite.body.velocity.x = 150
+  } else if (sprite.body.touching.right) {
+    sprite.body.velocity.x = -150
+  }
+}
+
+function bumpIntoPeople(sprite1, sprite2) {
+  if (sprite1.body.y > sprite2.body.y + 30) {
+    sprite1.bringToTop()
+  } else if (sprite1.body.y + 30 < sprite2.body.y) {
+    sprite1.sendToBack()
+  } else {
+    blockMoving(sprite1)
   }
 }
 
@@ -76,14 +123,18 @@ const playState = {
   },
   create: function () {
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
-    createNPCs(playState)
+    createGuests(playState)
     createPlayer(playState)
     cursors = this.game.input.keyboard.createCursorKeys()
     this.game.stage.backgroundColor = '#400000'
+    timer = this.game.time.now
   },
   update: function () {
     playerMovement()
-    this.game.physics.arcade.overlap(player, npcs, bumpIntoPeople, null, this)
+    if (this.game.time.now - timer > 1000) {
+      guestsMovement(playState)
+    }
+    this.game.physics.arcade.overlap(player, guests, bumpIntoPeople, null, this)
   }
 }
 
