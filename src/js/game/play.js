7cd0a17a4b09e5d47c9e941f57096ee1ff6b8797
Marcos <uchodc@gmail.com>
2018-02-01 10:12:46 +0000

    More guests movement madness

--- src/js/game/play.js
+++ src/js/game/play.js
@@ -25,6 +25,7 @@ function createPlayer(playState) {
 function playerMovement() {
   player.body.velocity.x = 0
   player.body.velocity.y = 0
+  player.sendToBack()
   if (cursors.left.isDown) {
     player.body.velocity.x = -150
     player.animations.play('walkwest')
@@ -75,10 +76,11 @@ function guestsMovement(game) {
 }
 
 function generateGuests(playState) {
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 0; i < 15; i += 1) {
     const guest = guests.create(playState.game.world.randomX, playState.game.world.randomY, 'guestmale1')
     guest.anchor.set(0.5, 0.5)
     guest.body.collideWorldBounds = true
+    guest.body.inmmovable = true
     guest.animations.add('walksouth', [0], 30, true)
     guest.animations.add('walknorth', [1], 30, true)
     guest.animations.add('walkwest', [2], 30, true)
@@ -93,27 +95,50 @@ function createGuests(playState) {
   generateGuests(playState)
 }
 
+/*
 function blockMoving(char) {
   const sprite = char
   if (sprite.body.touching.up) {
-    sprite.body.velocity.y = 150
+    sprite.body.velocity.y = 75
   } else if (sprite.body.touching.down) {
-    sprite.body.velocity.y = -150
+    sprite.body.velocity.y = -75
   } else if (sprite.body.touching.left) {
-    sprite.body.velocity.x = 150
+    sprite.body.velocity.x = 75
   } else if (sprite.body.touching.right) {
-    sprite.body.velocity.x = -150
+    sprite.body.velocity.x = -75
+  }
+}
+*/
+
+function getPerspective(y) {
+  const z = y
+  return z
+}
+
+function blockGuest(npc) {
+  if (npc.body.touching.up) {
+    npc.body.velocity.y = 0
+  } else if (npc.body.touching.down) {
+    npc.body.velocity.y = 0
+  } else if (npc.body.touching.left) {
+    npc.body.velocity.x = 0
+  } else if (npc.body.touching.right) {
+    npc.body.velocity.x = 0
   }
 }
 
-function bumpIntoPeople(sprite1, sprite2) {
-  if (sprite1.body.y > sprite2.body.y + 30) {
-    sprite1.bringToTop()
-  } else if (sprite1.body.y + 30 < sprite2.body.y) {
-    sprite1.sendToBack()
-  } else {
-    blockMoving(sprite1)
+function bumpBetweenGuests(sprite1, sprite2) {
+  if (sprite1.body.y < sprite2.body.y) {
+    sprite1.moveDown()
+    sprite2.moveUp()
+  } else if (sprite1.body.y > sprite2.body.y) {
+    sprite1.moveUp()
+    sprite2.moveDown()
   }
+  /*
+  } else {
+    blockGuest(sprite1)
+  */
 }
 
 const playState = {
@@ -134,8 +159,17 @@ const playState = {
     if (this.game.time.now - timer > 1000) {
       guestsMovement(playState)
     }
-    this.game.physics.arcade.overlap(player, guests, bumpIntoPeople, null, this)
+    // this.game.physics.arcade.overlap(player, guests, bumpIntoPeople, null, this)
+    this.game.physics.arcade.overlap(guests, guests, bumpBetweenGuests, null, this)
   }
+  /*
+  ,
+  render: function(){
+    guests.forEachAlive(function(item){
+      playState.game.debug.body(item)
+    })
+  }
+  */
 }
 
 export default playState
