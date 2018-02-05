1bc8a308a2ad820560136d73c15c5bccbc266604
Marcos <uchodc@gmail.com>
2018-02-05 00:06:00 +0000

    Docs & fixed gameOver timer

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist/js/'),
-    publicPath: '/dist/js/'
+    publicPath: 'js/'
   },
   module: {
     loaders: [
